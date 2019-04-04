79bb56f9d3c86c5dc3cd89e311c1dffd8c0c2903
unknown <pepegonzalez.hdz@gmail.com>
2019-04-04 01:09:46 -0600

    update

--- assets/app/custom/general/crud/metronic-datatable/base/html-table.js
+++ assets/app/custom/general/crud/metronic-datatable/base/html-table.js
@@ -7,7 +7,8 @@ var KTDatatableHtmlTableDemo = function() {
 	// demo initializer
 	var demo = function() {
 
-		var datatable = $('.kt-datatable').KTDatatable({
+		var datatable = $('#tabla1').KTDatatable({
+			pagination: false,
 			data: {
 				saveState: {cookie: false},
 			},
@@ -73,6 +74,150 @@ var KTDatatableHtmlTableDemo = function() {
 			],
 		});
 
+		var datatable2 = $('#tabla2').KTDatatable({
+			pagination: false,
+			data: {
+				saveState: {cookie: false},
+			},
+			search: {
+				input: $('#generalSearch'),
+			},
+			columns: [
+				{
+					field: 'idarticulo',
+					type: 'number',
+					width: 200,
+				},
+				{
+					field: 'descarticulo',
+					type: 'text',
+					width: 300,
+				},
+				{
+					field: 'um',
+					type: 'number',
+					width: 40,
+				},
+				{
+					field: 'Cantidad',
+					type: 'number',
+					width: 70,
+				},
+				{
+					field: 'OrderDate',
+					type: 'date',
+					format: 'YYYY-MM-DD',
+				}, {
+					field: 'Status',
+					title: 'Status',
+					autoHide: false,
+					// callback function support for column rendering
+					template: function(row) {
+						var status = {
+							1: {'title': 'Pending', 'class': 'kt-badge--brand'},
+							2: {'title': 'Delivered', 'class': ' kt-badge--danger'},
+							3: {'title': 'Canceled', 'class': ' kt-badge--primary'},
+							4: {'title': 'Success', 'class': ' kt-badge--success'},
+							5: {'title': 'Info', 'class': ' kt-badge--info'},
+							6: {'title': 'Danger', 'class': ' kt-badge--danger'},
+							7: {'title': 'Warning', 'class': ' kt-badge--warning'},
+						};
+						return '<span class="kt-badge ' + status[row.Status].class + ' kt-badge--inline kt-badge--pill">' + status[row.Status].title + '</span>';
+					},
+				}, {
+					field: 'Type',
+					title: 'Type',
+					autoHide: false,
+					// callback function support for column rendering
+					template: function(row) {
+						var status = {
+							1: {'title': 'Online', 'state': 'danger'},
+							2: {'title': 'Retail', 'state': 'primary'},
+							3: {'title': 'Direct', 'state': 'success'},
+						};
+						return '<span class="kt-badge kt-badge--' + status[row.Type].state + ' kt-badge--dot"></span>&nbsp;<span class="kt-font-bold kt-font-' +status[row.Type].state + '">' +	status[row.Type].title + '</span>';
+					},
+				},
+			],
+		});
+
+		var datatable3 = $('#tabla3').KTDatatable({
+			pagination: false,
+			data: {
+				saveState: {cookie: false},
+			},
+			search: {
+				input: $('#generalSearch'),
+			},
+			columns: [
+				{
+					field: 'idarticulo',
+					type: 'number',
+					width: 200,
+				},
+				{
+					field: 'descarticulo',
+					type: 'text',
+					width: 300,
+				},
+				{
+					field: 'um',
+					type: 'number',
+					width: 40,
+				},
+				{
+					field: 'Cantidad',
+					type: 'number',
+					width: 70,
+				},
+				{
+					field: 'OrderDate',
+					type: 'date',
+					format: 'YYYY-MM-DD',
+				}, {
+					field: 'Status',
+					title: 'Status',
+					autoHide: false,
+					// callback function support for column rendering
+					template: function(row) {
+						var status = {
+							1: {'title': 'Pending', 'class': 'kt-badge--brand'},
+							2: {'title': 'Delivered', 'class': ' kt-badge--danger'},
+							3: {'title': 'Canceled', 'class': ' kt-badge--primary'},
+							4: {'title': 'Success', 'class': ' kt-badge--success'},
+							5: {'title': 'Info', 'class': ' kt-badge--info'},
+							6: {'title': 'Danger', 'class': ' kt-badge--danger'},
+							7: {'title': 'Warning', 'class': ' kt-badge--warning'},
+						};
+						return '<span class="kt-badge ' + status[row.Status].class + ' kt-badge--inline kt-badge--pill">' + status[row.Status].title + '</span>';
+					},
+				}, {
+					field: 'Type',
+					title: 'Type',
+					autoHide: false,
+					// callback function support for column rendering
+					template: function(row) {
+						var status = {
+							1: {'title': 'Online', 'state': 'danger'},
+							2: {'title': 'Retail', 'state': 'primary'},
+							3: {'title': 'Direct', 'state': 'success'},
+						};
+						return '<span class="kt-badge kt-badge--' + status[row.Type].state + ' kt-badge--dot"></span>&nbsp;<span class="kt-font-bold kt-font-' +status[row.Type].state + '">' +	status[row.Type].title + '</span>';
+					},
+				},
+			],
+		});
+
+		var datatable4 = $('#tabla4').KTDatatable({
+			pagination: false,
+			data: {
+				saveState: {cookie: false},
+			},
+			search: {
+				input: $('#generalSearch'),
+			},
+		});
+
     $('#kt_form_status').on('change', function() {
       datatable.search($(this).val().toLowerCase(), 'Status');
     });
@@ -96,4 +241,7 @@ var KTDatatableHtmlTableDemo = function() {
 
 jQuery(document).ready(function() {
 	KTDatatableHtmlTableDemo.init();
+	$(document).find("#tabla1").css("min-height","0px");
+	$(document).find("#tabla2").css("min-height","0px");
+	$(document).find(".kt-datatable").css("margin-bottom","0px");
 });
